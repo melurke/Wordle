7b12f15aad046ad143046638bb059c7582c444db
melurke <standartbrostv@gmail.com>
2022-08-09 17:34:19 +0200

    Update game.py

--- game.py
+++ game.py
@@ -26,22 +26,24 @@ with open("word_list_complete.txt", "r") as file:
 word_list.sort()
 word_list_complete.sort()
 
-solution = random.choice(word_list)
-won_game = False
+while True:
+    solution = random.choice(word_list)
+    won_game = False
 
-for num_of_guesses in range(1, 7):
-    guess = ""
-    while not guess in word_list_complete:
-        guess = input(f"\nWhat is your {num_of_guesses}. guess? ").lower()
-        if not guess in word_list_complete:
-            print("The guess is not valid. Please try again:")
-    clue = generateClue(guess, solution)
-    print(f"The clue is {clue}")
-    if clue == "GGGGG":
-        print(f"Congratulations! You won the game in {num_of_guesses} guesses!")
-        won_game = True
-        break
-    num_of_guesses += 1
+    for num_of_guesses in range(1, 7):
+        guess = ""
+        while not guess in word_list_complete:
+            guess = input(f"\nWhat is your {num_of_guesses}. guess? ").lower()
+            if not guess in word_list_complete:
+                print("The guess is not valid. Please try again:")
+        clue = generateClue(guess, solution)
+        print(f"The clue is {clue}")
+        if clue == "GGGGG":
+            print(f"Congratulations! You won the game in {num_of_guesses} guesses!")
+            won_game = True
+            break
+        num_of_guesses += 1
 
-if not won_game:
-    print(f"\nYou have run out of guesses! The solution was {solution}.")
+    if not won_game:
+        print(f"\nYou have run out of guesses! The solution was {solution}.")
+    print("\n\n------------------------------------")
